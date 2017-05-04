0df47c53b68ae5bf3800e7a9bdc161ced2761187
Ammar Faizi <ammarfaizi2@gmail.com>
2017-05-04 22:43:35 +0700

    Fix variable

--- App/Brainly.php
+++ App/Brainly.php
@@ -24,7 +24,7 @@ class Brainly
 	
 	public function prepare($text,$limit=100)
 	{
-		$this->text = urlencode($text);
+		$this->text = $text;
 		$this->limit = (int) $limit;
 		$this->data = file_exists(data.'/brainly/data.txt') ? json_decode(data.'/brainly/data.txt') : array();
 		$this->data = $this->data===null ? array() : $this->data;
@@ -38,13 +38,13 @@ class Brainly
 		if (file_exists($this->file)) {
 			$a = json_decode(file_get_contents($this->file),true);
 		} else {
-			$ch = new CM_Curl('https://brainly.co.id/api/28/api_tasks/suggester?limit='.($this->limit).'&query='.$this->text);
+			$ch = new CM_Curl('https://brainly.co.id/api/28/api_tasks/suggester?limit='.($this->limit).'&query='.urlencode($this->text));
 			$a = json_decode($ch->execute(),true);
 			file_put_contents($this->file, json_encode($a,128));
 		}
 		foreach ($a['data']['tasks']['items'] as $key => $val) {
 			$que = trim(strip_tags(html_entity_decode($val['task']['content'],ENT_QUOTES,'UTF-8')));
-			similar_text($que, $input, $percent);
+			similar_text($que, $this->text, $percent);
 			$sim[$key] = $percent;
 		}
 		$result = $a[array_search(max($sim),$sim)];
