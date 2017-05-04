3dffd5d222ada309a390005d2bba15dd435db5d9
Ammar Faizi <ammarfaizi2@gmail.com>
2017-05-04 19:18:43 +0700

    Fix ChitChat class

--- loader.php
+++ loader.php
@@ -1,4 +1,6 @@
 <?php
+define('data',__DIR__.'/data');
+is_dir(data) or mkdir(data);
 spl_autoload_register(
         function ($class) {
             require __DIR__ . str_replace('\\', '/', $class).'.php';
