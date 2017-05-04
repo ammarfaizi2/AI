9ffde434c8a4e935b357083672f1c08f88fe7d6a
Ammar Faizi <ammarfaizi2@gmail.com>
2017-05-04 19:34:40 +0700

    Fix loader

--- loader.php
+++ loader.php
@@ -3,6 +3,6 @@ define('data',__DIR__.'/data');
 is_dir(data) or mkdir(data);
 spl_autoload_register(
         function ($class) {
-            require __DIR__ . str_replace('\\', '/', $class).'.php';
+            require __DIR__ . '/' .str_replace('\\', '/', $class).'.php';
         }
     );
