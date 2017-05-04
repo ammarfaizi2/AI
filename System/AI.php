30b151b0524a8c5b5ca50b2339422af92886ce1b
Ammar Faizi <ammarfaizi2@gmail.com>
2017-05-04 19:33:47 +0700

    Debugging

--- System/AI.php
+++ System/AI.php
@@ -2,7 +2,7 @@
 namespace System;
 defined('data') or die('Error : data not defined !');
 use System\Crayner_System;
-
+use App\ChitChat;
 /**
 * @author Ammar Faizi <ammarfaizi2@gmail.com> https://www.facebook.com/ammarfaizi2
 * @license RedAngel PHP Concept
@@ -14,12 +14,19 @@ class AI extends Crayner_System
     private $msg;
     private $absmsg;
     private $actor;
+    private $data;
+    private $chitchat;
+    private $reply;
     public function __construct()
     {
         is_dir(data.self::data) or mkdir(data.self::data);
+        $this->data = data.self::data;
+        $this->chitchat = file_exists($this->data.'chit_chat_on');
     }
+
     /**
     *   @param string,string
+    *   @return instance
     */
     public function prepare($text,$actor=null)
     {
@@ -28,12 +35,34 @@ class AI extends Crayner_System
         $this->actor    = $actor;
         return $this;
     }
+
+    /**
+    *
+    */
     public function status()
     {
 
     }
+
+    /**
+    *   @return boolean
+    */
     public function execute()
     {
-
+        if ($this->chitchat) {
+            $st = new ChitChat('Carik');
+            if($st->prepare($this->msg)->execute()){
+                $this->reply = $st->fetch_reply();
+                return $this->fetch_reply===null ? false : true;
+            } else {
+                return false;
+            }
+        } else {
+            return false;
+        }
+    }
+    public function fetch_reply()
+    {
+        return $this->fetch_reply;
     }
 }
--- act.php
+++ act.php
@@ -2,3 +2,7 @@
 require __DIR__ . '/loader.php';
 use System/AI;
 $ai = new AI();
+$msg = "halo carik";
+$act = "Ammar Faizi";
+$st = $ai->prepare($msg,$act);
+$st->execute();
