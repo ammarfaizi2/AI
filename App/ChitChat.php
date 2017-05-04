4aaeaf0ca5d32e7cb5a4273b9f4f124102acd97f
Ammar Faizi <ammarfaizi2@gmail.com>
2017-05-04 18:55:23 +0700

    Fix ChitChat execute method

--- App/ChitChat.php
+++ App/ChitChat.php
@@ -1,18 +1,36 @@
 <?php
 namespace App;
 use System\CM_Curl;
+defined('data') or die('data Constant is not defined !');
+
 /**
-* 
+* @author Ammar Faizi <ammarfaizi2@gmail.com> https://www.facebook.com/ammarfaizi2
+* @license RedAngel PHP Concept
 */
+
 class ChitChat
 {
+	const data = '/chitchat/';
 	private $ai_name;
+	private $msg;
+	private $cur;
 	public function __construct($ai_name='Carik')
 	{
 		$this->ai_name 	= strtolower($ai_name);
+		$this->cur_file = data.self::data.'currentchat';
+		$this->cur 		= file_exists($this->cur_file) ? (int) file_get_contents($this->cur_file) : 1;
 	}
 	public function prepare($text)
 	{
-		$ch = new CM_Curl();
+		$this->msg = urlencode($text);
+		return $this;
+	}
+	public function execute()
+	{
+		$ch = new CM_Curl("http://www.simsimi.com/getRealtimeReq?uuid=j5AgOtBpNvzg8B5pRWYPnQhl2qktGLYbTC9LxXNCDyj&lc=en&ft=1&reqText=".($this->msg)."&status=W");
+		$ch->set_header(array(
+				'Cookie: _ga=GA1.2.1766465947.1489671555; uuid=j5AgOtBpNvzg8B5pRWYPnQhl2qktGLYbTC9LxXNCDyj; currentChatCnt='.($this->cur).'; _gid=GA1.2.1282686541.1493896281; _gat=1'
+			));
+		$rt = json_decode($ch->execute(),true);
 	}
 }
