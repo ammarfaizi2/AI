315bbf6c33495d57b17d2f630bf8627b4dd30e7c
Ammar Faizi <ammarfaizi2@gmail.com>
2017-05-05 18:34:24 +0700

    Update concept

--- act.php
+++ act.php
@@ -1,9 +1,11 @@
 <?php
-require __DIR__ . '/loader.php';
+require __DIR__.'/vendor/autoload.php';
+define('data',__DIR__.'/data');
+is_dir(data) or mkdir(data);
 use System\AI;
 
 $ai = new AI();
-$msg = "ask penemu lampu";
+$msg = "translate penemu lampu";
 $act = "Ammar Faizi";
 $st = $ai->prepare($msg, $act);
 $st->execute();
--- loader.php
+++ loader.php
@@ -1,8 +1,2 @@
 <?php
-define('data', __DIR__.'/data');
-is_dir(data) or mkdir(data);
-spl_autoload_register(
-        function ($class) {
-            require __DIR__ . '/' .str_replace('\\', '/', $class).'.php';
-        }
-    );
+require __DIR__.'/vendor/autoload.php';
