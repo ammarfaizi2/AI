29799a9dfc112622ddca3990bc1d076cdfc10260
Ammar Faizi <ammarfaizi2@gmail.com>
2017-05-10 21:17:46 +0700

    Fix MyAnimeList

--- src/App/MyAnimeList.php
+++ src/App/MyAnimeList.php
@@ -1,6 +1,7 @@
 <?php
 namespace App;
 
+use Curl\CMCurl;
 /**
 * @author Ammar F. <ammarfaizi2@gmail.com> https://www.facebook.com/ammarfaizi2
 * @license RedAngel PHP Concept 2017
@@ -11,15 +12,21 @@ class MyAnimeList
     private $option;
     public function __construct($user, $pass)
     {
+        is_dir(data.'/ani/myanimelist') or mkdir(data.'/ani/myanimelist');
+        is_dir(data.'/ani/myanimelist/results') or mkdir(data.'/ani/myanimelist/results');
         $this->option = array(CURLOPT_USERPWD=>"{$user}:{$pass}",CURLOPT_CONNECTTIMEOUT=>30);
     }
     public function search($query, $type=null)
     {
-        $ch = new CMCurl("https://myanimelist.net/api/".($type===null?"anime":$type)."/search.xml?q=".urlencode($query));
-        $ch->set_option($this->option);
-        $result = $ch->execute();
-        $ch->close();
-        $a = simplexml_load_string();
-        return $a;
+        if (file_exists(data.'/ani/myanimelist/results/'.md5($query.$type))) {
+            $result = file_get_contents(data.'/ani/myanimelist/results/'.md5($query.$type));
+        } else {
+            $ch = new CMCurl("https://myanimelist.net/api/".($type===null?"anime":$type)."/search.xml?q=".urlencode($query));
+            $ch->set_option($this->option);
+            $result = $ch->execute();
+            $ch->close();
+            file_put_contents(data.'/ani/myanimelist/results/'.md5($query.$type), $result);
+        }
+        return simplexml_load_string($result);
     }
 }
