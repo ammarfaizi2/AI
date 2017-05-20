64e31b938ac35520e10b951759f4c5719826a6dc
Ammar Faizi <ammarfaizi2@gmail.com>
2017-05-20 16:03:00 +0700

    Optimize code with some property

--- src/System/Chat.php
+++ src/System/Chat.php
@@ -29,6 +29,9 @@ trait Chat
 		}
 		private function chat()
 		{
+			$this->exms = explode(' ',$this->msg);
+			$this->cword = count($this->exms);
+			$this->mslg = strlen($this->msg);
 			foreach($this->wl as $key => $val){
 				if($r=$this->{'check'.$val[0]}($val[1],$val[2],$val[3])){
 					
@@ -41,7 +44,10 @@ trait Chat
 		*/
 		private function check1($wordlist,$wordcheck,$maxwords,$maxlength)
 		{
-			
+			if(self::max_($this->mslg,$maxlength) or self::max_($this->cword,$maxwords)){
+				return false;
+			} else
+			if(self::max_())
 		}
 		private static function max_(int $n,int $max)
 		{
