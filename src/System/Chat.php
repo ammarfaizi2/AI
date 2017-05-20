419f7b1dcd550850e2aa28c40b99c0bf45d06dfa
Ammar Faizi <ammarfaizi2@gmail.com>
2017-05-20 15:49:43 +0700

    Improve Chat Trait

--- src/System/Chat.php
+++ src/System/Chat.php
@@ -14,18 +14,32 @@ trait Chat
 		{
 			/**
 			*
+			* hai,hay,hi,hy
+			*		mode,answer,wordcheck
 			*/
 			$this->wl = array(
 				'hai,hay,hi,hy'=>array(
-				
-				)
+					1,array(),true,
+				),
 			
 			
 			
 			
 			);
 		}
-		private function chat
+		private function chat()
+		{
+			foreach($this->wl as $key => $val){
+				if($r=$this->{'check'.$val[0]}($val[1],$val[2],$val[3])){
+					
+				}
+			}
+		}
+		
+		/**
+		*		@param wordlist
+		*/
+		private function check1($wordlist)
 		{
 			
 		}
