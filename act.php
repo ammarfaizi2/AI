2f9ec93c1fefb7495dbcf841625293b2a4cf6678
Ammar Faizi <ammarfaizi2@gmail.com>
2017-05-27 11:42:04 +0700

    Delete unused flow

--- act.php
+++ act.php
@@ -6,7 +6,7 @@ use System\AI;
 
 $ai = new AI();
 $act = "Ammar Faizi";
-$msg = "i_anime 31765";
+$msg = "hitung 100+200";
 $st = $ai->prepare($msg, $act);
 $st->execute();
 var_dump($st->fetch_reply());
