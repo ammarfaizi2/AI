b904e53c6c56c4d5c22c5c006bf0233af4b0d970
Ammar Faizi <ammarfaizi2@gmail.com>
2017-05-27 11:58:56 +0700

    Create JadwalSholat class

---
+++ src/App/JadwalSholat.php
@@ -0,0 +1,32 @@
+<?php
+namespace App;
+
+defined('data') or die('Error : data not defined !');
+
+use Curl\CMCurl;
+
+/**
+* @author Ammar Faizi <ammarfaizi2@gmail.com>
+*/
+
+class JadwalSholat
+{
+	/**
+	*	@var	string	Bulan sekarang
+	*/
+	private $bulan;
+
+	public function __construct()
+	{	
+		$this->bulan = strtolower(date('M'));
+		is_dir(data.'/jadwal_sholat') or mkdir(data.'/jadwal_sholat');
+	}
+
+	/**
+	*	@param	string	$kota	Pilih kota
+	*/
+	public function get_jadwal($kota)
+	{
+
+	}
+}
