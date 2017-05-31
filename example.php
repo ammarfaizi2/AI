1321ae068bf29886329458ecb5a3ce2170f95f35
Ammar Faizi <ammarfaizi2@gmail.com>
2017-05-31 15:39:02 +0700

    Update example

--- example.php
+++ example.php
@@ -1,12 +1,17 @@
 <?php
 require __DIR__ . '/vendor/autoload.php';
+
+/**
+ * Define `data` constant and create folder for AI data
+ */
 define('data', __DIR__.'/data');
 is_dir(data) or mkdir(data);
+
 use AI\AI;
 
 $ai = new AI();
-$act = "Ammar Faizi";
-$msg = "sekarang bulan apa?";
+$actor = "Ammar Faizi";
+$msg   = "sekarang bulan apa?";
 $st = $ai->prepare($msg, $act);
 $st->execute();
 var_dump($st->fetch_reply());
