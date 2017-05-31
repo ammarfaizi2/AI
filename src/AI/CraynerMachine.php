dad28af284908c7688390dfc47ce280990ca4d74
Ammar Faizi <ammarfaizi2@gmail.com>
2017-05-31 16:16:06 +0700

    Update class

--- src/AI/CraynerMachine.php
+++ src/AI/CraynerMachine.php
@@ -2,6 +2,7 @@
 namespace AI;
 
 use AI\CraynerSystem;
+use AI\Hub\Singleton;
 
 /**
  * @author Ammar Faizi <ammarfaizi2@gmail.com>
@@ -9,6 +10,7 @@ use AI\CraynerSystem;
 
 class CraynerMachine extends CraynerSystem
 {
+	use Singleton;
     public function __construct()
     {
     }
