a4750d55b6d011a50263fc5c47baa674a212d588
Ammar Faizi <ammarfaizi2@gmail.com>
2017-05-31 16:15:35 +0700

    Update CraynerSystem

--- src/AI/CraynerSystem.php
+++ src/AI/CraynerSystem.php
@@ -13,5 +13,20 @@ abstract class CraynerSystem
     {
     }
 
+    private function chat()
+    {
+    }
+
+    private function command()
+    {
+    }
+
+    private function root_command()
+    {
+    }
+
     abstract public function set_timezone();
+    abstract public function prepare(string $text);
+    abstract public function execute();
+    abstract public function fetch_reply();
 }
