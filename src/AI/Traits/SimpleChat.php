9e418c9ba5f67cd88550c02df4c63be7fec3c71a
Ammar Faizi <ammarfaizi2@gmail.com>
2017-07-06 17:08:59 +0700

    Improve SimpleChat trait

--- src/AI/Traits/SimpleChat.php
+++ src/AI/Traits/SimpleChat.php
@@ -2,10 +2,16 @@
 
 namespace AI\Traits;
 
+/**
+ * @author Ammar Faizi <ammarfaizi2@gmail.com>
+ */
+
 trait SimpleChat
 {
 	private function simple_chat()
 	{
-
+		foreach (\AI\CY\ChatST::$wordlist as $key => $value) {
+			# code...
+		}
 	}
 }
