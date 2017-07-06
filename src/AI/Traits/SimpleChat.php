8d56a6bb6105bbb00ef4e9d63afc1b81f9f4fd32
Ammar Faizi <ammarfaizi2@gmail.com>
2017-07-06 17:26:36 +0700

    Prepare ChatST

--- src/AI/Traits/SimpleChat.php
+++ src/AI/Traits/SimpleChat.php
@@ -10,8 +10,23 @@ trait SimpleChat
 {
 	private function simple_chat()
 	{
-		foreach (\AI\CY\ChatST::$wordlist as $key => $value) {
-			# code...
+		foreach (\AI\CY\ChatST::$wordlist as $k => $v) {
+			if ($this->compare($this->input, $k, $v['t'][0], $v['t'][1])) {
+				# code...
+			}
 		}
+	}
+
+	/**
+	 * @param string $input
+	 * @param string $key
+	 * @param bool	 $word_match
+	 * @param int	 $max_length	Max length to response.
+	 * @param int	 $max_words		Max words to response.
+	 * @param bool   $time_reply	
+	 */
+	private function compare($input, $key, $word_match = false, $max_length = null, $max_words = null, $time_reply = false)
+	{
+
 	}
 }
