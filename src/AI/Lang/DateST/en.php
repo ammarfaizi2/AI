6fd217b833e8a7e389c60b8122731b2ae74ae932
Ammar Faizi <ammarfaizi2@gmail.com>
2017-07-07 06:41:15 +0700

    Fix DateST temporary

---
+++ src/AI/Lang/DateST/en.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace AI\Lang\DateST;
+
+/**
+ * @author Ammar Faizi <ammarfaizi2@gmail.com>
+ */
+
+class en
+{
+    public static $day = [
+        "Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"
+    ];
+    public static $month = [
+        "", "January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"
+    ];
+}
--- src/AI/Lang/DateST/id.php
+++ src/AI/Lang/DateST/id.php
@@ -9,7 +9,7 @@ namespace AI\Lang\DateST;
 class id
 {
     public static $day = [
-        "", "Senin", "Selasa", "Rabu", "Kamis", "Jum'at", "Sabtu"
+        "Minggu", "Senin", "Selasa", "Rabu", "Kamis", "Jum'at", "Sabtu"
     ];
     public static $month = [
         "", "Januari", "Februari", "Maret", "April", "Mei", "Juni", "Juli", "Agustus", "September", "Oktober", "November", "Desember"
