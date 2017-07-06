01035b8d9bdf80670bb87da0f73dd8c88c0517d1
Ammar Faizi <ammarfaizi2@gmail.com>
2017-07-06 17:05:06 +0700

    Update namespace

---
+++ src/AI/CY/Error.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace AI\CY;
+
+class Error
+{
+	public static $errno = [
+		"error" => 1,
+    	"error_constants" => 2,
+    	"error_simplecmd_not_found" => 3,
+    ];
+}
---
+++ src/AI/Traits/SimpleChat.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace AI\Traits;
+
+trait SimpleChat
+{
+	private function simple_chat()
+	{
+
+	}
+}
