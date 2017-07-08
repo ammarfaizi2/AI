5f34be73d576aa41c3dd7c1dbdc6ea1801bf3f6a
Ammar Faizi <ammarfaizi2@gmail.com>
2017-07-08 21:09:29 +0700

    Update config.php

--- config.php
+++ config.php
@@ -12,10 +12,10 @@ if (!defined("data")) {
 /**
  * L Virtual.
  */
-if (!defined("PHPVIRTUAL_DIR")) {
+/*//
 	define("PHPVIRTUAL_URL", "https://webhooks.redangel.ga/virtual/php");
 	define("PHPVIRTUAL_DIR", "/home/ammar/web/webhooks/public/virtual/php");
 	define("JAVAVIRTUAL_DIR", "/home/ammar/web/webhooks/public/virtual/java");
 	define("RUBYVIRTUAL_DIR", "/home/ammar/web/webhooks/public/virtual/ruby");
 	define("CVIRTUAL_DIR", "/home/ammar/web/webhooks/public/virtual/c");
-}
+//*/
